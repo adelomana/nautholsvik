0c0411004fd8254fc30b4f7f3b0c8c59f3fec483
Adrian <adrianlopezgarciadelomana@gmail.com>
2022-08-17 16:21:00 +0000

    fixed error on drop_duplicates

--- annotation/annotation.R
+++ annotation/annotation.R
@@ -13,7 +13,7 @@ annotation_file = "/home/adrian/projects/nautholsvik/results/annotation/annotati
 
 # annotation defined from sleuth walk through, https://pachterlab.github.io/sleuth_walkthroughs/trapnell/analysis.html
 mart = biomaRt::useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host='https://uswest.ensembl.org')
-t2g = biomaRt::getBM(attributes=c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart=mart)
+t2g = biomaRt::getBM(attributes=c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name", 'description', 'hgnc_symbol'), mart=mart)
 t2g = dplyr::rename(t2g, target_id=ensembl_transcript_id, ens_gene=ensembl_gene_id, ext_gene=external_gene_name)
 
 write.csv(t2g, annotation_file)
